319be6a5f5419266143032a8446a2575eb878111
Fernad Corbin <60511629+frndcrbn@users.noreply.github.com>
2023-02-10 01:17:46 +0800

    Delete onlineshoppingsystem directory

--- onlineshoppingsystem/Customer.java
+++
@@ -1,4 +0,0 @@
-package onlineshoppingsystem;
-
-public class Customer {
-}
--- onlineshoppingsystem/Program.java
+++
@@ -1,8 +0,0 @@
-package onlineshoppingsystem;
-
-public class Program {
-
-    public static void main(String[] args) {
-
-    }
-}
